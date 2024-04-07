0d76567c540ab0edbddd43bd3d5a2e69ac080fdd
vh8154 <144638698+vh8154@users.noreply.github.com>
2024-04-07 19:20:37 -0400

    comment

--- courses.py
+++ courses.py
@@ -73,36 +73,37 @@ def regdetails():
     try:
         gen_table, prof_table, dept_table = class_details(classid)
         department_table = []
-
+        try:
         #grabs the correct departments and professors
-        for row3 in dept_table:
-            if gen_table[0][0] == row3[0]:
-                department_table.append([row3[1], row3[2]])
-        professors_table = []
-        for row2 in prof_table:
-            if gen_table[0][0] == row2[0]:
-                professors_table.append(row2[1])
-        html_code = flask.render_template('regdetails.html',
-            classid=classid, general_table=gen_table[0],
-            prof_table=professors_table,
-            dept_table=department_table, dept=dept,
-            area=area, coursenum=num, title=title)
-        response = flask.make_response(html_code)
-        return response
-
-    #handles no classid error
-    except Exception as ex:
-        if str(ex) == ("cannot unpack non-iterable" +
-            " OperationalError object"):
-            html_code = flask.render_template('error_details.html'
-                , ex = ex, message = "A server error occurred. "
-            + "Please contact the system administrator.")
+            for row3 in dept_table:
+                if gen_table[0][0] == row3[0]:
+                    department_table.append([row3[1], row3[2]])
+            professors_table = []
+            for row2 in prof_table:
+                if gen_table[0][0] == row2[0]:
+                    professors_table.append(row2[1])
+            html_code = flask.render_template('regdetails.html',
+                classid=classid, general_table=gen_table[0],
+                prof_table=professors_table,
+                dept_table=department_table, dept=dept,
+                area=area, coursenum=num, title=title)
+            response = flask.make_response(html_code)
+            return response
+        except Exception as ex:
+            #classid error
+            html_code = flask.render_template('error_details.html',
+                ex = ex, message = "no class with classid "
+                + classid + " exists")
             print(str(ex), file=sys.stderr)
             response = flask.make_response(html_code)
             return response
-        html_code = flask.render_template('error_details.html',
-            ex = ex, message = "no class with classid "
-            + classid + " exists")
+
+    #handles database errors
+    except Exception as ex:
+        html_code = flask.render_template('error_details.html'
+            , ex = ex, message = "A server error occurred. "
+        + "Please contact the system administrator.")
         print(str(ex), file=sys.stderr)
         response = flask.make_response(html_code)
         return response
+        
